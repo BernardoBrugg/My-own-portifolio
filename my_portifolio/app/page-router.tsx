5f49febb251a003502f91c8c902660021566609b
BernardoBrugg <bbbrugg@gmail.com>
2025-12-02 23:59:23 -0300

    fixing mobile problem

--- my_portifolio/app/page-router.tsx
+++ my_portifolio/app/page-router.tsx
@@ -11,7 +11,17 @@ export default function PageRouter() {
 
   useEffect(() => {
     const checkMobile = () => {
-      setIsMobile(window.innerWidth < 768);
+      const userAgent =
+        navigator.userAgent || navigator.vendor || (window as { opera?: string }).opera || '';
+      const isMobileDevice =
+        /android|avantgo|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+          userAgent.toLowerCase()
+        );
+
+      const isSmallScreen = window.innerWidth < 768;
+      const hasTouch = "ontouchstart" in window || navigator.maxTouchPoints > 0;
+
+      setIsMobile(isMobileDevice || (isSmallScreen && hasTouch));
     };
 
     checkMobile();
